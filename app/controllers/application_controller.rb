5fd28b618bd27c0d1bae5344d249d81abcfc5339
Larrykj <larrykings2002@gmail.com>
2026-02-20 12:21:04 +0300

    Fix CI/CD: Add PostgreSQL service to test jobs and fix API controller inheritance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
-  class API < ApplicationController
+  class API < ActionController::API
     # API-specific configuration can go here
   end
 end
