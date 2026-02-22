1a082e41055797246bf407d09f2e353f213f00de
Larrykj <larrykings2002@gmail.com>
2026-02-22 15:17:27 +0300

    Fix RuboCop offenses: Correct ArticlesController syntax, fix indentation and formatting

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,15 +1,12 @@
 module Api
     class ArticlesController < ApplicationController
-        before_action :set_article, only: [:show, :edit, :update, :destroy]
-        before_action :require_login, except: [:index, :show]
-        def index
+    before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+    before_action :require_login, except: [ :index, :show ]
             @articles = Article.all
         end
 
-        def show
-            @article = Article.find([params:id])
-        end
-         
+        def show; end
+
         def new
             @article = Article.new
         end
@@ -19,76 +16,37 @@ module Api
             if @article.save
                 redirect_to @article, notice: "Article Created!"
             else
-                render :new, status:unprocessable_entity
+                render :new, status: :unprocessable_entity
             end
         end
 
-        def edit
-            @article = Article.find(params[:id])
-        end
+        def edit; end
 
         def update
-            @article = Article.find(params[:id])
             if @article.update(article_params)
                 redirect_to @article, notice: "Article updated"
             else
                 render :edit, status: :unprocessable_entity
+            end
+        end
+
+        def destroy
+            @article.destroy
+            redirect_to articles_path, notice: "Article deleted!"
+        end
+
+        private
+
+        def article_params
+            params.require(:article).permit(:title, :body, :published)
+        end
+
+        def set_article
+            @article = Article.find(params[:id])
+        end
+
+        def require_login
+            redirect_to login_path unless current_user
         end
     end
-
-    def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
-        redirect_to articles_path, notice: "Article deleted!"
-    end
-
-    private
-
-    def article_params
-        params.require(:article).permit(:title, :body)
-    end
-
-    def show
-        # URL: /articles/42
-        @article = Article.find(params[:id]) # params[:id] => "42"
-    end
-
-    def index
-        # URL: /articles?sort=recent
-        @sort = params[:sort] # => "recent"
-    end
-
-    private
-
-    def article_params
-        params.require(:article).permit(:title, :body, :published)
-    end
-
-    def show
-        #@article is already set!
-    end
-
-    def edit
-         # @article is already set!
-    end
-
-    private
-
-    def set_article
-        @article = Article.find(params[:id])
-    end
-
-    def require_login
-        redirect_to login_path unless current_user
-    end
-
-    def show
-        @article = Article.find(params[:id])
-
-        respond_to do |format|
-            format.html # renders show.html.erb
-            format.json { render json: @article }
-            format.xml { render xml: @article }
-        end
-end 
 end
--- app/controllers/api/revisions_controller.rb
+++ app/controllers/api/revisions_controller.rb
@@ -2,11 +2,11 @@ module Api
   class RevisionsController < ApplicationController
     def calculate_even
       number = (params[:calculate_even] || params[:compute]).to_i
-            parity = calculate_convert(number)
+      parity = calculate_convert(number)
 
-            render json: {
-                number: number,
-                parity: parity,
+      render json: {
+        number: number,
+        parity: parity,
         is_even: number.even?
       }
     end
