787f414ad02250a6fb8c444e395001edbd03f7e9
Larrykj <larrykings2002@gmail.com>
2026-02-24 13:07:06 +0300

    Added Test Functionality

--- test/controllers/api/reversed_strings_controller_test.rb
+++ test/controllers/api/reversed_strings_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class Api::ReversedStringsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return reversed string" do
+    get "/api/process_string", params: { input_string: "Hello" }
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_equal "Hello", json_response["original"]
+    assert_equal "olleH", json_response["reversed_string"]
+  end
+
+  test "should handle empty string" do
+    get "/api/process_string", params: { input_string: "" }
+    assert_response :success
+    json_response = JSON.parse(response.body)
+    assert_equal "", json_response["original"]
+    assert_equal "", json_response["reversed_string"]
+  end
 end
