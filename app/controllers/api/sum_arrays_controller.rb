aaafe3a32c08e0d39d83098ed3d70b6d699b0d8b
Larrykj <larrykings2002@gmail.com>
2026-02-25 08:20:39 +0300

    program that takes an array of integers and returns the sum of all elements

--- app/controllers/api/sum_arrays_controller.rb
+++ app/controllers/api/sum_arrays_controller.rb
@@ -1,12 +1,19 @@
 module Api
-  class SumArraysController < ApplicationController
-    def calculate_array_sum
-      values = Array(params[:sum_arrays]).map(&:to_i)
+    class SumArraysController < ApplicationController
+        def calculate_array_sum
+            numbers = Array(params[:sum_arrays]).map(&:to_i)
+            total = calculate_sum(numbers)
 
-      render json: {
-        sum_arrays: values,
-        total: values.sum
-      }
+            render json: {
+                sum_arrays: numbers,
+                total: total
+            }
+        end
+
+        private
+
+        def calculate_sum(numbers)
+            numbers.sum
+        end
     end
-  end
 end
---
+++ test/controllers/api/sum_arrays_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+module Api
+  class SumArraysControllerTest < ActionDispatch::IntegrationTest
+    test "should calculate array sum" do
+      get api_calculate_array_sum_url(sum_arrays: [1, 2, 3, 4, 5])
+
+      assert_response :success
+      response_json = JSON.parse(response.body)
+      assert_equal [1, 2, 3, 4, 5], response_json["sum_arrays"]
+      assert_equal 15, response_json["total"]
+    end
+
+    test "should handle invalid values as zero" do
+      get api_calculate_array_sum_url(sum_arrays: ["4", "abc", "6"])
+
+      assert_response :success
+      response_json = JSON.parse(response.body)
+      assert_equal [4, 0, 6], response_json["sum_arrays"]
+      assert_equal 10, response_json["total"]
+    end
+  end
+end
