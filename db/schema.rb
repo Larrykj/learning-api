31b8d657f2b893d5c37a686211611723dd93e712
Larrykj <larrykings2002@gmail.com>
2026-03-03 19:41:36 +0300

    Created the migrations for cars, car_owners and owners

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_03_155325) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "authors", force: :cascade do |t|
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "author"
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "car_owners", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "car_id", null: false
+    t.datetime "created_at", null: false
+    t.uuid "owner_id", null: false
+    t.datetime "ownership_end_date", precision: nil
+    t.datetime "ownership_start_date", precision: nil
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_car_owners_on_car_id"
+    t.index ["owner_id"], name: "index_car_owners_on_owner_id"
+  end
+
+  create_table "cars", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.text "make"
+    t.text "model"
+    t.datetime "updated_at", null: false
+    t.integer "year"
+  end
+
+  create_table "owners", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.text "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "books", "authors"
+  add_foreign_key "car_owners", "cars"
+  add_foreign_key "car_owners", "owners"
 end
